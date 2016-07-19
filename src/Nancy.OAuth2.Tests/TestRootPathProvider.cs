11244fd86f2424df36220b78bde9380754ce9805
Steven Atkinson <steven.atkinson@gmx.com>
2016-07-19 09:05:03 +0100

    Trying to fix issues when locating views.

--- src/Nancy.OAuth2.Tests/TestRootPathProvider.cs
+++ src/Nancy.OAuth2.Tests/TestRootPathProvider.cs
@@ -1,10 +1,16 @@
-﻿namespace Nancy.OAuth2.Tests
+﻿using System.IO;
+
+namespace Nancy.OAuth2.Tests
 {
     internal class TestRootPathProvider : IRootPathProvider
     {
+        private static string _rootPath;
+
         public string GetRootPath()
         {
-            return "../..";
+            var rootPath = _rootPath ?? (_rootPath = Path.GetFullPath(Directory.GetCurrentDirectory() + "/../.."));
+
+            return rootPath;
         }
     }
 }
