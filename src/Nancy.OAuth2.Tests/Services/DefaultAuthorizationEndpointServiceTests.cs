2f42995dfb897881cf5faa08f2451dba4e7554ed
Steven Atkinson <steven.atkinson@gmx.com>
2016-09-28 12:14:09 +0100

    Added tests for the default services.

---
+++ src/Nancy.OAuth2.Tests/Services/DefaultAuthorizationEndpointServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Nancy.OAuth2.Models;
+using Nancy.OAuth2.Services;
+using NUnit.Framework;
+
+namespace Nancy.OAuth2.Tests.Services
+{
+    [TestFixture]
+    [Parallelizable]
+    public class DefaultAuthorizationEndpointServiceTests
+    {
+        private IAuthorizationEndpointService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new DefaultAuthorizationEndpointService();
+        }
+
+        [Test]
+        public void GenerateAuthorizationToken_ShouldThrow_NotImplementedException()
+        {
+            var request = new AuthorizationRequest();
+            var context = new NancyContext();
+
+            Assert.Throws<NotImplementedException>(() => _service.GenerateAuthorizationToken(request, context));
+        }
+
+        [Test]
+        public void ValidateRequest_ShouldThrow_NotImplementedException()
+        {
+            var request = new AuthorizationRequest();
+            var context = new NancyContext();
+
+            Assert.Throws<NotImplementedException>(() => _service.ValidateRequest(request, context));
+        }
+
+        [Test]
+        public void GetAuthorizationView_ShouldThrow_NotImplementedException()
+        {
+            var request = new AuthorizationRequest();
+            var context = new NancyContext();
+
+            Assert.Throws<NotImplementedException>(() => _service.GetAuthorizationView(request, context));
+        }
+    }
+}
